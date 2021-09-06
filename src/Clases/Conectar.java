f764ae806c731ba25e57263d9b5a8a24aa79ce4c
juliocaba <84859345+juliocaba@users.noreply.github.com>
2021-09-06 18:48:29 -0300

    a
    a

---
+++ src/Clases/Conectar.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author julio caba
+ */
+public class Conectar {
+     Connection cn;  
+    /* constructor */
+    public Connection conexion() {
+        try {            
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection("jdbc:mysql://localhost/bdinforme","root","");
+            System.out.println("conectado");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return cn;
+    }
+}
